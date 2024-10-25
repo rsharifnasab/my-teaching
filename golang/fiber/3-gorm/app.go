6c9bdd757ef384b46bcf0459bbd3845b660e76b5
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-10-25 09:45:20 +0330

    add book example

---
+++ golang/fiber/3-gorm/app.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm-postgres/database"
+	"gorm-postgres/routes"
+)
+
+func setUpRoutes(app *fiber.App) {
+	app.Get("/hello", routes.Hello)
+	app.Get("/allbooks", routes.AllBooks)
+
+	app.Post("/addbook", routes.AddBook)
+	app.Post("/book", routes.Book)
+
+	app.Put("/update", routes.Update)
+
+	app.Delete("/delete", routes.Delete)
+}
+
+func main() {
+	database.ConnectDb()
+	app := fiber.New()
+
+	setUpRoutes(app)
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ golang/fiber/3-gorm/database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"log"
+
+	"gorm-postgres/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+// connectDb
+func ConnectDb() {
+	dsn := "postgres://postgres:postgres@127.0.0.1:5432/pgsql?search_path=public&sslmode=disable"
+
+	println(dsn)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Book{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
