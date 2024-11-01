030ca6df4f3226fc0349a9d066172f90d9bb29ba
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-11-01 15:24:48 +0330

    class modifications cors

--- golang/fiber/12-cors/main.go
+++ golang/fiber/12-cors/main.go
@@ -10,22 +10,20 @@ import (
 
 func main() {
 	apiApp := fiber.New()
-
 	apiApp.Use(logger.New())
-
 	apiApp.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000", //, http://127.0.0.1:3000",
-		AllowMethods: "GET",
+		// allow xdomain in y backend
+		AllowOrigins: "http://localhost:3000,http://127.0.0.1:3000",
+		AllowMethods: "GET,POST",
 	}))
-
 	apiApp.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("This is data from the API server. (GET)")
 	})
 	apiApp.Post("/api", func(c *fiber.Ctx) error {
 		return c.SendString("This is data from the API server. (POST)")
 	})
-
 	go func() {
+		// ydomain (destination): localhost:4000
 		log.Fatal(apiApp.Listen(":4000"))
 	}()
 
@@ -33,4 +31,5 @@ func main() {
 	apiApp.Use(logger.New())
 	clientApp.Static("/", "./client")
 	log.Fatal(clientApp.Listen(":3000"))
+	// xdomain (request source): 127.0.0.1:3000
 }
