20dd086c7be30d1adf2f4964b592b7170442842e
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-11-01 09:51:27 +0330

    stateless

---
+++ golang/fiber/12-stateless/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+var global int
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		global = global + 1
+
+		result := fmt.Sprintf("number: %d\n", global)
+		return c.SendString(result)
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
